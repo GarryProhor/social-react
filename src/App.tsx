57edd190be0496698bd277395f9004ec55fa56c4
igorp <igorprohorchenko@gmail.com>
2022-12-08 16:30:12 +0300

    project preparation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/global.css';
+import {Home} from "./pages/Home";
 
 function App() {
+
   return (
-    <div className="App">
-     Hello
-    </div>
+      <Home/>
   );
 }
 
