eb8f19aae6a3ff72856d36696d3298e37f439cf9
igorp <igorprohorchenko@gmail.com>
2023-06-11 16:02:24 +0300

    UPD LoginModal

--- src/features/login/LoginModal/LoginModal.tsx
+++ src/features/login/LoginModal/LoginModal.tsx
@@ -4,16 +4,22 @@ import './LoginModal.css';
 import {LoginModalTop} from "../LoginModalTop/LoginModalTop";
 import {LoginFormOne} from "../LofinForms/LoginFormOne";
 
+import {RootState} from "../../../redux/Store";
+import {useSelector} from "react-redux";
+
 interface LoginModalProps {
     toggleModal: ()=>void;
 }
 
 export const LoginModal:React.FC<LoginModalProps> = ({toggleModal}) => {
+
+    const state = useSelector((state:RootState) => state.user);
+
     return (
         <Modal
             topContent={<LoginModalTop closeModal={toggleModal} />}
-            content={<LoginFormOne/>}
-            bottomContent={<div>Login bottom</div>}
+            content={state.userName ? <>Login form 2</> : <LoginFormOne/>}
+            bottomContent={<div>Login form 2 bottom</div>}
         />
     );
 };
