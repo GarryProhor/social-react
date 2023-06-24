21275277726e2177c9689c71412f319b6760573f
igorp <igorprohorchenko@gmail.com>
2023-06-24 15:23:59 +0300

    UPD login modal

--- src/features/login/LoginModal/LoginModal.tsx
+++ src/features/login/LoginModal/LoginModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Modal} from "../../../components/Modal/Modal";
 import './LoginModal.css';
 import {LoginModalTop} from "../LoginModalTop/LoginModalTop";
@@ -6,6 +6,7 @@ import {LoginFormOne} from "../LofinForms/LoginFormOne";
 
 import {RootState} from "../../../redux/Store";
 import {useSelector} from "react-redux";
+import {LoginFormTwo} from "../LofinForms/LoginFormTwo";
 
 interface LoginModalProps {
     toggleModal: ()=>void;
@@ -15,10 +16,16 @@ export const LoginModal:React.FC<LoginModalProps> = ({toggleModal}) => {
 
     const state = useSelector((state:RootState) => state.user);
 
+    const [password, setPassword] = useState<string>("");
+
+    const handlePassword = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
+
     return (
         <Modal
             topContent={<LoginModalTop closeModal={toggleModal} />}
-            content={state.userName ? <>Login form 2</> : <LoginFormOne/>}
+            content={state.userName ? <LoginFormTwo setPassword={handlePassword}/> : <LoginFormOne/>}
             bottomContent={<div>Login form 2 bottom</div>}
         />
     );
