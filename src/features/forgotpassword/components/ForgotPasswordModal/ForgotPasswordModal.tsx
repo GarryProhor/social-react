3beb7236b53294e02812d7295fa7ca6d869372e3
GarryProhor <igorprohorchenko@gmail.com>
2023-07-09 09:32:45 -0800

    ADD: ForgotPasswordModal

---
+++ src/features/forgotpassword/components/ForgotPasswordModal/ForgotPasswordModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Modal} from "../../../../components/Modal/Modal";
+
+export const ForgotPasswordModal:React.FC<{toggleModal:()=>void}> = ({toggleModal}) => {
+    return (
+        <Modal
+            topContent={<>Top</>}
+            content={<>Content</>}
+            bottomContent={<>Bottom</>}
+        />
+    );
+};
