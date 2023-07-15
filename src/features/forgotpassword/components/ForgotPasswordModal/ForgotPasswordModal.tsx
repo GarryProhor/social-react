660df80d8258bda45c4429f1eebd25bcea3853b6
GarryProhor <igorprohorchenko@gmail.com>
2023-07-15 11:08:41 -0800

    UPD: forgot password modal

--- src/features/forgotpassword/components/ForgotPasswordModal/ForgotPasswordModal.tsx
+++ src/features/forgotpassword/components/ForgotPasswordModal/ForgotPasswordModal.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Modal} from "../../../../components/Modal/Modal";
+import {ForgotModalTop} from "../ForgotModalTop/ForgotModalTop";
+import {ForgotFormOne} from "../ForgotForms/ForgotFormOne";
+import {validatedEmail, validatePhone} from "../../../../services/Validators";
+import axios from "axios";
 
 export const ForgotPasswordModal:React.FC<{toggleModal:()=>void}> = ({toggleModal}) => {
+
+    const [credential, setCredential] = useState<string>('');
+    const [error, setError] = useState<boolean>(false);
+    const [step, setStep] = useState<number>(1);
+
+    const changeCredential = (credential:string) => {
+        setCredential(credential);
+    }
+
+    const searchUser = async () =>{
+        let findUserDTO = {
+            email: '',
+            phone: '',
+            userName: ''
+        };
+
+        if(validatedEmail(credential)){
+            findUserDTO = {
+                ...findUserDTO,
+                email: credential,
+            }
+        }else if(validatePhone(credential)){
+            findUserDTO = {
+                ...findUserDTO,
+                phone: credential,
+            }
+        }else {
+            findUserDTO = {
+                ...findUserDTO,
+                userName: credential,
+            }
+        }
+
+        try {
+            setError(false);
+            let res = await axios.post('http://localhost:8080/auth/find', findUserDTO)
+            let data = await res.data;
+            console.log(data)
+            setStep(2);
+        }catch (e) {
+            setError(true);
+        }
+    }
+
     return (
         <Modal
-            topContent={<>Top</>}
-            content={<>Content</>}
-            bottomContent={<>Bottom</>}
+            topContent={<ForgotModalTop closeModal={toggleModal}/>}
+            content={<ForgotFormOne setCredential={changeCredential} error={error}/>}
+            bottomContent={step === 1 ? <button onClick={searchUser}>Search for user</button> : <></>}
         />
     );
 };
