f37691f45c451f5805eeb5d7a5f5aa5b22af3772
GarryProhor <igorprohorchenko@gmail.com>
2023-08-27 11:38:25 -0800

    ADD: determineForgotButton method

--- src/features/forgotpassword/utils/ForgotPasswordUtils.tsx
+++ src/features/forgotpassword/utils/ForgotPasswordUtils.tsx
@@ -1,5 +1,8 @@
 import {ForgotFormOne} from "../components/ForgotForms/ForgotFormOne";
 import {ForgotFormTwo} from "../components/ForgotForms/ForgotFormTwo";
+import exp from "constants";
+import {ForgotButtonOne} from "../components/ForgotButtonOne/ForgotButtonOne";
+import {ForgotButtonTwo} from "../components/ForgotButtonTwo/ForgotButtonTwo";
 
 export const determineForgotFormContent = (step: number, setCredential: (value: string) => void, error: boolean,
                                            email: string, phone: string): JSX.Element => {
@@ -8,6 +11,18 @@ export const determineForgotFormContent = (step: number, setCredential: (value: 
             return <ForgotFormOne setCredential={setCredential} error={error}/>
         case 2:
             return <ForgotFormTwo email={email} phone={phone}/>
+        case 3:
+            return <></>
+    }
+    return <></>
+}
+
+export const determineForgotButton = (step: number, credential: string, searchUser: () => void, cancel: () => void, sendCode: () => void):JSX.Element => {
+    switch (step){
+        case 1:
+            return <ForgotButtonOne value={credential} handleClick={searchUser} />
+        case 2:
+            return <ForgotButtonTwo onCancel={cancel} sendCode={sendCode} />
         case 3:
             return <></>
     }
